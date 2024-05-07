678bc35d4cc98e2202aadeb8f59915b9e875a8ca
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2024-05-07 20:49:37 -0300

    Create: página inicial

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,31 @@
+import "./styles/app.css"
+
+//Components
+import Question from "./Components/Question";
 
 export default function App() {
 
   return (
-    <>
-      hello world
-    </>
+    <div className="wrapper">
+      <h1>perguntas</h1>
+
+      <form className="post-question-form">
+        <div className="post-question-input">
+          <input type="text" required/>
+          <span>faça sua pergunta</span>
+        </div>
+
+        <button type="submit">
+          <img alt="Interrogação" src="/question.svg"/>
+          perguntar
+        </button>
+
+      </form>
+      
+      <div className="questions-container">
+        <Question/>
+        <Question/>
+      </div>
+    </div>
   )
 }
-
