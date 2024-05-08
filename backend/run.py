bec802ed4d141f4dcac68f98ec2eed90e269c353
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2024-05-08 12:26:56 -0300

    refactor: Alterando forma de inicialização da aplicação

--- backend/run.py
+++ backend/run.py
@@ -1,1 +1,10 @@
+from dotenv import dotenv_values
 from app import app
+
+config = dotenv_values('.flaskenv')
+PORT=config.get('PORT') or 3000
+
+if __name__ == "__main__": 
+  app.run(port=PORT)
+  print(f"APP running on port {PORT}")
+  
