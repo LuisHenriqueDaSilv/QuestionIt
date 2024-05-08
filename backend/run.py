da95d4a48dd55695bbfd7aba0c638feef1ee1cff
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2024-05-08 17:55:27 -0300

    fix: permitindo requisições de fora do container

--- backend/run.py
+++ backend/run.py
@@ -5,6 +5,5 @@ config = dotenv_values('.flaskenv')
 PORT=config.get('PORT') or 3000
 
 if __name__ == "__main__": 
-  app.run(port=PORT)
-  print(f"APP running on port {PORT}")
+  app.run(port=PORT, host="0.0.0.0")
   
