37998e29555e87711b7de25848b52ac91f9e8ef4
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2024-05-09 17:55:01 -0300

    update: initialization system

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,7 @@
-from dotenv import dotenv_values
+import os
 from app import app
 
-config = dotenv_values('.flaskenv')
-PORT=config.get('PORT') or 3000
+PORT=os.environ.get("PORT", 3000)#Default: 3000
 
 if __name__ == "__main__": 
   app.run(port=PORT, host="0.0.0.0")
-  
