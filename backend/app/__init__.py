f68546e068e857f150948c37e706cc3f2124a9a6
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2024-05-09 17:53:41 -0300

    feat: setup database/database-migrations settings

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_migrate import Migrate
+from dotenv import load_dotenv, dotenv_values
+
 from app.routes import router
+from app.database import db
+from app.database.models import Question, Response #Used in migrations
+
 
 app = Flask(__name__)
+load_dotenv('.env')
+envConfigs = dotenv_values()
+app.config.from_mapping(envConfigs) # Settings from env var
 
+# Database/Database-migrations setup:
+db.init_app(app)
+migrate = Migrate(app, db, directory='./app/database/migrations')
+
+#Routes:
 app.register_blueprint(router)
