66bb62130197ec999969fd248ff3ded8c680563b
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2024-05-10 11:44:22 -0300

    refactor

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,7 @@
 import os
 from app import app
 
-PORT=os.environ.get("PORT", 3000)#Default: 3000
 
+PORT=os.environ.get("PORT", 3000)#Default: 3000
 if __name__ == "__main__": 
   app.run(port=PORT, host="0.0.0.0")
