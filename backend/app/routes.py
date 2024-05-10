b7fa7abbf948d449271b814319608a570d2e31af
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2024-05-10 12:18:03 -0300

    feat: creating routes to create and list questions and responses

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from app.controllers import QuestionControler
+from app.controllers import QuestionController, ResponseController
 
 router = Blueprint(
   'router',
@@ -9,10 +9,24 @@ router = Blueprint(
 
 @router.get("/")
 def index():
-  return {
-    "msg": "hello world"
-  }
+  return {"msg": "hello world"}
 
 @router.post("/question/create")
-def create():
-  return QuestionControler.create()
+def questionCreate():
+  return QuestionController.create()
+
+@router.get("/question/<id>")
+def questionRead(id):
+  return QuestionController.read(id)
+
+@router.get("/question/list")
+def questionList():
+  return QuestionController.list()
+
+@router.post("/response/create")
+def responseCreate():
+  return ResponseController.create()
+
+@router.get("/response/list")
+def responseList():
+  return ResponseController.list()
